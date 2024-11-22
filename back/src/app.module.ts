ace3f897b171c6dec5074bb32a2b35ba19c24a2c
Gastygas <gastonrodrigogonzalez55@gmail.com>
2024-11-22 01:00:26 -0300

    connect to DataBase by TypeORM

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import typeormConfig from './config/typeOrm'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [typeormConfig] }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
